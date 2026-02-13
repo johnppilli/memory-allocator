2b01ac12cbc81560feb969b4b1ce01f9c7d691a9
John Pilli <pilli7869@gmail.com>
2026-02-13 14:47:12 -0800

    changed to c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h> //imports the standard I/O library 
 #include <stdbool.h> //allows you to work with bool
 
-
+char memory[POOL_SIZE];
 
 
 int main() { //entry point 
@@ -16,7 +16,13 @@ struct Block {
 };
 
 
+void* my_malloc(size_t size) { //my_malloc = function that finds a free spot, and then returns to tell you where   
+    //Code goes here
+}  //without the *, C wouldn't give back an address and it woudl return nothing
 
+void my_free(void* ptr){  //
+    //Code for it goes here
+}
 
 
 
