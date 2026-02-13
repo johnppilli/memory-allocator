2f44c838d877b51ee9255be44293f1425dffd8e6
John Pilli <pilli7869@gmail.com>
2026-02-12 18:14:28 -0800

    adding comments

--- main.zig
+++ main.zig
@@ -1,15 +1,15 @@
 
 
-const std = @import("std");
+const std = @import("std"); //import Zig library
 
-pub fn main() void {
-    std.debug.print("memory alocator\n", .{};
+pub fn main() void {  //entry point of program, like main() in C
+    std.debug.print("memory alocator\n", .{}; //this will just print this text to the terminal
 }
 
 
 const Blcok = struct {
-    size: usize, 
-    is_free: bool, 
+    size: usize, //stores how big the memory block is, unsigned integer
+    is_free: bool, //tracks whether this block is available or taken
 };
 
 
