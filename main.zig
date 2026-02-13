afd5c8b8397899d1d6348803fdf1587e482c1ab9
John Pilli <pilli7869@gmail.com>
2026-02-12 18:11:59 -0800

    foundations

--- main.zig
+++ main.zig
@@ -5,3 +5,15 @@ const std = @import("std");
 pub fn main() void {
     std.debug.print("memory alocator\n", .{};
 }
+
+
+const Blcok = struct {
+    size: usize, 
+    is_free: bool, 
+};
+
+
+
+
+
+
