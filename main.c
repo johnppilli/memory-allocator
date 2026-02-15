2eba4715a3db27b7e493bf8ea959e6904ad5a05b
John Pilli <pilli7869@gmail.com>
2026-02-14 20:48:44 -0800

    creating my_malloc

--- main.c
+++ main.c
@@ -4,18 +4,23 @@
 #include <stdio.h> //imports the standard I/O library 
 #include <stdbool.h> //allows you to work with bool
 
-char memory[POOL_SIZE];
+#define POOL_SIZE 1024 //the total size of memory pool in bytes
+char memory [POOL_SIZE]; //the memory pool that the allocator manages
+
+struct Block {
+    size_t size; //unsigned integer for memory sizes
+    bool is_free; //1 = free, 0 = taken 
+};
 
 
 int main() { //entry point 
     printf("memory allocator\n");
-    return 0; //should tell the OS that everything is fine
 
     void* a = my_malloc(32); //asks for 32 bytes
     void* b = my_malloc(64); //ask for 64 more bytes
 
-    printf("a: %p\n, a"); //print teh address
-    printf("b: %p\n, b"); //should be a different address
+    printf("a: %p\n", a); //print teh address
+    printf("b: %p\n", b); //should be a different address
 
     my_free(a); //free the first one
 
@@ -26,14 +31,12 @@ int main() { //entry point
 }
 
 
-struct Block {
-    size_t size; //unsigned integer for memory sizes
-    bool is_free; //1 = free, 0 = taken 
-};
-
 
 void* my_malloc(size_t size) { //my_malloc = function that finds a free spot, and then returns to tell you where   
-    //Code goes here
+    
+    void* current = memory; 
+
+
 }  //without the *, C wouldn't give back an address and it woudl return nothing
 
 void my_free(void* ptr){  //
