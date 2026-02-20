c393819b8ff39b2fffcd769f0f200faa60f202b6
John Pilli <pilli7869@gmail.com>
2026-02-19 16:08:52 -0800

    added error handling test

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main() {
     my_free(a); //free it - now there's a 64-byte free block
 
     void* b = my_malloc(16); //ask for only 16 - should split the 64 into 16 + leftover
-    void* c - my_malloc(16); //should use the leftover from the split
+    void* c = my_malloc(16); //should use the leftover from the split
     printf("b: %p\n", b);
     printf("c: %p\n", c); //b and c should be different but close together
 
@@ -36,6 +36,12 @@ int main() {
     printf("f: %p\n", f); //shoudl reuse d's old address
 
 
+// ============ TEST 3 ================
+    printf("=== Test 3: Error handling ===\n");
+    void* g = my_malloc(2000); //bigger than the whole pool
+    if (g == NULL) {
+        printf("g: allocation failed (no space)\n");
+    }
 
     return 0;
 }
